32e9a44bd48d8ca7f90f4fc0901f2bc470c183bf
postbox-18 <aravinthc@gmail.com>
2022-01-28 12:12:47 +0530

    updating ui

--- app/src/main/java/com/examples/batterylevel/wbutton/ButtonActivity.java
+++ app/src/main/java/com/examples/batterylevel/wbutton/ButtonActivity.java
@@ -14,20 +14,27 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.examples.batterylevel.BatteryWidget;
 import com.examples.batterylevel.R;
+import com.google.android.material.progressindicator.CircularProgressIndicator;
 
 import me.itangqi.waveloadingview.WaveLoadingView;
+import pl.pawelkleczkowski.customgauge.CustomGauge;
 
 public class ButtonActivity extends AppCompatActivity {
     int i;
     boolean isCharging, usbCharge, acCharge;
-    WaveLoadingView mWaveLoadingView;
-    TextView txtbattery_status, txtstatus, txtcharge_status, txtport;
+    WaveLoadingView mWaveLoadingView,txtbattery_status;
+    TextView  txtstatus, txtcharge_status, txtport,volTextView,text_level;
     private int progressStatus = 0;
     private String TAG="BatteryLevel";
+    CustomGauge progress_bar;
+    int batteryVol;
+    float fullVoltage;
+    String currentBatteryVol="Current Battery Voltage :";
     private Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,9 +61,13 @@ public class ButtonActivity extends AppCompatActivity {
         });
 
         mWaveLoadingView = (WaveLoadingView) findViewById(R.id.waveLoadingView);
+        txtbattery_status = (WaveLoadingView) findViewById(R.id.txtbattery_status);
+        progress_bar = findViewById(R.id.progress_bar);
         mWaveLoadingView.setShapeType(WaveLoadingView.ShapeType.CIRCLE);
-        txtbattery_status= (TextView) findViewById(R.id.txtbattery_status);
+        txtbattery_status.setShapeType(WaveLoadingView.ShapeType.RECTANGLE);
         txtstatus= (TextView) findViewById(R.id.txtstatus);
+        text_level= (TextView) findViewById(R.id.text_level);
+        volTextView= (TextView) findViewById(R.id.volTextView);
         txtcharge_status= (TextView) findViewById(R.id.txtcharge_status);
         txtport= (TextView) findViewById(R.id.txtport);
         BroadcastReceiver broadcastReceiverBattrery = new BroadcastReceiver() {
@@ -65,6 +76,9 @@ public class ButtonActivity extends AppCompatActivity {
                 Integer integerBatteryLevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL,0);
                 i= Integer.parseInt(integerBatteryLevel.toString());
 
+                batteryVol = (int)(intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE,0));
+                fullVoltage = (float) (batteryVol * 0.001);
+                volTextView.setText(fullVoltage+" volt");
                 //Log.e("Battery","BatteryLevel"+i);
               /*  if(i>=50 && i<=75){
                     txtstatus.setText("Battery Status: Moderate");
@@ -96,29 +110,44 @@ public class ButtonActivity extends AppCompatActivity {
                     mWaveLoadingView.setWaveColor(Color.parseColor("#FF26FF00"));
                     mWaveLoadingView.setBorderColor(Color.parseColor("#FF26FF00"));
                 } */
+
                 if (i<10){
                     txtstatus.setText("Critically Low, Please Charge the Device!");
                     mWaveLoadingView.setWaveColor(Color.parseColor("#FC0404"));
                     mWaveLoadingView.setBorderColor(Color.parseColor("#D40C1C"));
+
+                    txtbattery_status.setWaveColor(Color.parseColor("#FC0404"));
+                    txtbattery_status.setBorderColor(Color.parseColor("#D40C1C"));
                 }
                 else if (i>=10 && i<=20){
                     txtstatus.setText("LOW");
                     mWaveLoadingView.setWaveColor(Color.parseColor("#FC0412"));
                     mWaveLoadingView.setBorderColor(Color.parseColor("#FC0404"));
+                    txtbattery_status.setWaveColor(Color.parseColor("#FC0412"));
+                    txtbattery_status.setBorderColor(Color.parseColor("#FC0404"));
+
                 }
                 else if (i>20 && i<=50){
                     txtstatus.setText("Draining");
                     mWaveLoadingView.setWaveColor(Color.parseColor("#FCCC04"));
                     mWaveLoadingView.setBorderColor(Color.parseColor("#F4BB13"));
+                    txtbattery_status.setWaveColor(Color.parseColor("#FCCC04"));
+                    txtbattery_status.setBorderColor(Color.parseColor("#F4BB13"));
+
                 }else if (i>50 && i<=80){
                     txtstatus.setText("Moderate");
+                    txtbattery_status.setWaveColor(Color.parseColor("#94EC94"));
+                    txtbattery_status.setBorderColor(Color.parseColor("#4BBC54"));
                     mWaveLoadingView.setWaveColor(Color.parseColor("#94EC94"));
                     mWaveLoadingView.setBorderColor(Color.parseColor("#4BBC54"));
+
                 }
                 else if (i>80 && i<=100){
                     txtstatus.setText("GOOD");
                     mWaveLoadingView.setWaveColor(Color.parseColor("#04E474"));
                     mWaveLoadingView.setBorderColor(Color.parseColor("#04E474"));
+                    txtbattery_status.setWaveColor(Color.parseColor("#04E474"));
+                    txtbattery_status.setBorderColor(Color.parseColor("#04E474"));
                 }
 
 
@@ -139,6 +168,40 @@ public class ButtonActivity extends AppCompatActivity {
                 mWaveLoadingView.resumeAnimation();
                 mWaveLoadingView.cancelAnimation();
                 mWaveLoadingView.startAnimation();
+
+                txtbattery_status.setTopTitle(i+"%");
+                txtbattery_status.setCenterTitleColor(Color.WHITE);
+                txtbattery_status.setCenterTitle("");
+                txtbattery_status.setBottomTitleSize(18);
+                txtbattery_status.setProgressValue(0);
+                txtbattery_status.setBorderWidth(5);
+                txtbattery_status.setAmplitudeRatio(100);
+//                txtbattery_status.setWaveColor(Color.parseColor("#B2DFDB"));
+//                txtbattery_status.setBorderColor(Color.parseColor("#4CAF50"));
+                txtbattery_status.setTopTitleStrokeColor(Color.WHITE);
+                txtbattery_status.setTopTitleStrokeWidth(3);
+                txtbattery_status.setWaterLevelRatio(0.2f);
+                txtbattery_status.setAnimDuration(3000);
+                txtbattery_status.pauseAnimation();
+                txtbattery_status.resumeAnimation();
+                txtbattery_status.cancelAnimation();
+                txtbattery_status.startAnimation();
+
+//                progress_bar.setProgress(i);
+
+                progress_bar.setValue(i);
+                if(i<16) {
+                    progress_bar.setPointStartColor(Color.parseColor("#E42434"));
+                    progress_bar.setPointEndColor(Color.parseColor("#FC0404"));
+                }
+                else if(i>15 && i<31)
+                {
+                    progress_bar.setPointStartColor(Color.parseColor("#FCEC04"));
+                    progress_bar.setPointEndColor(Color.parseColor("#FCFB20"));
+                }
+                text_level.setText(String.valueOf(i)+"%");
+
+
                 new Thread(new Runnable() {
                     public void run() {
                         while (progressStatus < i) {
@@ -149,7 +212,7 @@ public class ButtonActivity extends AppCompatActivity {
                                 public void run() {
                                     mWaveLoadingView.setProgressValue(progressStatus);
                                     String x=String.valueOf(i);
-                                    txtbattery_status.setText(x+"%");
+                                    txtbattery_status.setProgressValue(progressStatus);
                                     getChargingLevel(getApplicationContext());
 
                                     //Battery charging conditions
@@ -185,7 +248,8 @@ public class ButtonActivity extends AppCompatActivity {
                 }).start();
 
                 String x=String.valueOf(i);
-                txtbattery_status.setText("Battery Level:"+x+"%");
+
+
                 getChargingLevel(getApplicationContext());
 
                 //Battery charging conditions
